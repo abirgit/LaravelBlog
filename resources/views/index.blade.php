ef2179119ce2a3f266d59c97e278a570511103d4
Abir Ahmed <abirahemd@gmail.com>
2021-09-28 16:28:05 +0600

    index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+                    Do you want to become a Mobile Automation Tester?
                 </h1>
                 <a href="/blog"
                 class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase ">
@@ -16,7 +16,7 @@
     </div>
     <div class="sm:grid grid-cols-2 gap-20 w-25 mx-auto py-15 border-b border-gray-200">
         <div>
-            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700"alt="">
+            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700"  alt="">
         </div>
         <div class="m-auto sm:m-auto text-left w-4/5 block">
             <h2 class="text-4xl font-extrabold text-gray-600">
